8ee7379bfb5cfdb735631b96c4040ad9e7a2ff70
ItzSomebody <23221108+ItzSomebody@users.noreply.github.com>
2018-07-13 16:05:49 -0700

    Some changes to number obfuscation transformer

--- src/main/java/me/itzsomebody/radon/transformers/misc/NumberObfuscation.java
+++ src/main/java/me/itzsomebody/radon/transformers/misc/NumberObfuscation.java
@@ -27,57 +27,64 @@ import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 
 /**
- * Transformer that splits up integers into simple arithmetic evaluations.
+ * Transformer that splits up integers into simple bitwise evaluations.
  *
  * @author ItzSomebody
- * @author VincBreaker (Sorry Vinc, I just had to steal the idea of Smoke's number obfuscation lol)
  */
 public class NumberObfuscation extends AbstractTransformer {
     /**
-     * Applies obfuscation.}.
+     * Applies obfuscation.
      */
     public void obfuscate() {
         AtomicInteger counter = new AtomicInteger();
         long current = System.currentTimeMillis();
         this.logStrings.add(LoggerUtils.stdOut("------------------------------------------------"));
-        this.logStrings.add(LoggerUtils.stdOut("Started number obfuscation transformer"));
-        this.classNodes().stream().filter(classNode -> !this.exempted(classNode.name, "Numbers")).forEach(classNode -> {
-            classNode.methods.stream().filter(methodNode ->
-                    !this.exempted(classNode.name + '.' + methodNode.name + methodNode.desc, "Numbers")
-                            && hasInstructions(methodNode)).forEach(methodNode -> {
-                for (AbstractInsnNode insn : methodNode.instructions.toArray()) {
-                    if (methodSize(methodNode) > 60000) break;
-                    if (BytecodeUtils.isIntInsn(insn)) {
-                        int originalNum = BytecodeUtils.getIntNumber(insn);
-                        int value1 = NumberUtils.getRandomInt();
-                        int value2 = originalNum ^ value1;
+        this.logStrings.add(LoggerUtils.stdOut("Started normal number obfuscation transformer"));
+        this.classNodes().parallelStream().filter(classNode -> !this.exempted(classNode.name, "Numbers")).forEach(classNode ->
+                classNode.methods.parallelStream().filter(methodNode ->
+                        !this.exempted(classNode.name + '.' + methodNode.name + methodNode.desc, "Numbers")
+                                && hasInstructions(methodNode)).forEach(methodNode -> {
+                    for (AbstractInsnNode insn : methodNode.instructions.toArray()) {
+                        if (methodSize(methodNode) > 60000) break;
+                        if (BytecodeUtils.isIntInsn(insn)) {
+                            int originalNum = BytecodeUtils.getIntNumber(insn);
+                            int value1 = NumberUtils.getRandomInt();
+                            int value2 = originalNum ^ value1;
 
-                        InsnList insnList = new InsnList();
-                        insnList.add(BytecodeUtils.getNumberInsn(value1));
-                        insnList.add(BytecodeUtils.getNumberInsn(value2));
-                        insnList.add(new InsnNode(IXOR));
+                            InsnList insnList = new InsnList();
+                            insnList.add(BytecodeUtils.getNumberInsn(value1));
+                            insnList.add(BytecodeUtils.getNumberInsn(NumberUtils.getRandomInt()));
+                            insnList.add(new InsnNode(SWAP));
+                            insnList.add(new InsnNode(DUP_X1));
+                            insnList.add(new InsnNode(POP2));
+                            insnList.add(BytecodeUtils.getNumberInsn(value2));
+                            insnList.add(new InsnNode(IXOR));
 
-                        methodNode.instructions.insertBefore(insn, insnList);
-                        methodNode.instructions.remove(insn);
-                        counter.incrementAndGet();
-                    } else if (BytecodeUtils.isLongInsn(insn)) {
-                        long originalNum = BytecodeUtils.getLongNumber(insn);
-                        long value1 = NumberUtils.getRandomLong();
-                        long value2 = originalNum ^ value1;
+                            methodNode.instructions.insertBefore(insn, insnList);
+                            methodNode.instructions.remove(insn);
+                            counter.incrementAndGet();
+                        } else if (BytecodeUtils.isLongInsn(insn)) {
+                            long originalNum = BytecodeUtils.getLongNumber(insn);
+                            long value1 = NumberUtils.getRandomLong();
+                            long value2 = originalNum ^ value1;
 
-                        InsnList insnList = new InsnList();
-                        insnList.add(BytecodeUtils.getNumberInsn(value1));
-                        insnList.add(BytecodeUtils.getNumberInsn(value2));
-                        insnList.add(new InsnNode(LXOR));
+                            InsnList insnList = new InsnList();
+                            insnList.add(BytecodeUtils.getNumberInsn(NumberUtils.getRandomLong()));
+                            insnList.add(BytecodeUtils.getNumberInsn(value1));
+                            insnList.add(new InsnNode(DUP2_X2));
+                            insnList.add(new InsnNode(POP2));
+                            insnList.add(new InsnNode(POP2));
+                            insnList.add(BytecodeUtils.getNumberInsn(value2));
+                            insnList.add(new InsnNode(LXOR));
 
-                        methodNode.instructions.insertBefore(insn, insnList);
-                        methodNode.instructions.remove(insn);
-                        counter.incrementAndGet();
+                            methodNode.instructions.insertBefore(insn, insnList);
+                            methodNode.instructions.remove(insn);
+                            counter.incrementAndGet();
+                        }
                     }
-                }
-            });
-        });
-        this.logStrings.add(LoggerUtils.stdOut("Split " + counter + " numbers into bitwise xor instructions."));
+                })
+        );
+        this.logStrings.add(LoggerUtils.stdOut("Split " + counter + " numbers into math instructions."));
         this.logStrings.add(LoggerUtils.stdOut("Finished. [" + tookThisLong(current) + "ms]"));
     }
 }
