37baffbee1568b183a372369bd22cbc3f31177bc
ItzSomebody <23221108+ItzSomebody@users.noreply.github.com>
2018-11-03 19:31:35 -0700

    Adds request in MissingClassException

--- radon-program/src/main/java/me/itzsomebody/radon/exceptions/MissingClassException.java
+++ radon-program/src/main/java/me/itzsomebody/radon/exceptions/MissingClassException.java
@@ -20,5 +20,7 @@ package me.itzsomebody.radon.exceptions;
 public class MissingClassException extends RuntimeException {
     public MissingClassException(String msg) {
         super(msg);
+        System.out.println("Do NOT report an issue about this exception unless you have absolutely made sure that" +
+                " the class reported missing exists in the library list you provided to Radon");
     }
 }
