5d3c7279f0c7764d4f99d9d1eaa2e7a20f19d5e8
undefined <mymcpe42@Gmail.com>
2019-03-03 16:19:04 +0800

    Fix ClassNotFound
    Enhanced the Power also!

--- radon-program/src/main/java/me/itzsomebody/radon/transformers/obfuscators/miscellaneous/FakeTryCatch.java
+++ radon-program/src/main/java/me/itzsomebody/radon/transformers/obfuscators/miscellaneous/FakeTryCatch.java
@@ -1,12 +1,16 @@
 package me.itzsomebody.radon.transformers.obfuscators.miscellaneous;
 
 import java.util.concurrent.atomic.AtomicInteger;
+
+import me.itzsomebody.radon.Main;
 import me.itzsomebody.radon.exclusions.ExclusionType;
 import me.itzsomebody.radon.transformers.Transformer;
 import me.itzsomebody.radon.utils.AccessUtils;
 import me.itzsomebody.radon.utils.LoggerUtils;
 import me.itzsomebody.radon.utils.StringUtils;
 
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
@@ -15,6 +19,19 @@ public class FakeTryCatch extends Transformer {
 	@Override
 	public void transform() {
 		AtomicInteger counter = new AtomicInteger();
+		AtomicInteger counter_Shiped = new AtomicInteger();
+
+		// Warnning!
+		// set it true
+		// to set Handler to class itself
+		boolean Crasher = false;
+		// it will got Verify Excep
+		// do -noverify to bypass
+
+		//Nice Naming
+		String FAKE_Handler = "////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////;//)//goto//break//META-INF//public//static//throw//throw//new//"
+				+ getANSI() + "//" + ")};" + getANSI() + StringUtils.randomAlphaNumericString(12) + '\u0000';
+		ClassNode FAKE_Node = this.genHandler(FAKE_Handler);
 
 		getClassWrappers().parallelStream().filter(classWrapper -> !excluded(classWrapper)).forEach(classWrapper -> {
 			ClassNode classNode = classWrapper.classNode;
@@ -26,41 +43,55 @@ public class FakeTryCatch extends Transformer {
 						.forEach(mn -> {
 							// Very Nice
 							if (mn.localVariables != null) {
-								for (int i = 1 + ((mn.localVariables.size() + 1) / 2); i > 0; i--) {
-									LabelNode begin = new LabelNode();
-									LabelNode handler = new LabelNode();
-									LabelNode end = new LabelNode();
-									if (mn.localVariables != null) {
-										mn.instructions.insert(begin);
-										mn.instructions.add(end);
-										mn.instructions.add(handler);
+								doobfu: {
+									for (int i = 1 + ((mn.localVariables.size() + 1) / 2); i > 0; i--) {
+										LabelNode begin = new LabelNode();
+										LabelNode handler = new LabelNode();
+										LabelNode end = new LabelNode();
 
-										// mn.instructions.add(new /
-										// LdcInsnNode(StringUtils.randomAlphaNumericString(11)));
+										// Ship the Method if it has athrow.
+										for (AbstractInsnNode ai = mn.instructions.getFirst(); ai != null; ai = ai
+												.getNext()) {
+											counter_Shiped.incrementAndGet();
+											if (Opcodes.ATHROW == ai.getOpcode())
+												break doobfu;
 
-										// mn.instructions.add(new InsnNode(Opcodes.POP2));
-										// mn.instructions.add(new InsnNode(Opcodes.POP));
+											counter_Shiped.decrementAndGet();
+										}
+										if (mn.localVariables != null) {
+											mn.instructions.insert(begin);
+											mn.instructions.add(end);
+											mn.instructions.insert(end, new LdcInsnNode(getANSI()));
 
-										mn.instructions.add(new LdcInsnNode(StringUtils.randomAlphaNumericString(16)));
-										mn.instructions.add(new InsnNode(Opcodes.ATHROW));
+											// Bypass Some deobfuscator
+											mn.instructions.add(handler);
+											mn.instructions.insert(handler, new LdcInsnNode(getANSI()));
 
-										LocalVariableNode ex = new LocalVariableNode(
-												StringUtils.randomAlphaNumericString(70),
-												"L" + "java/lang/Throwable" + ";", null, begin, handler,
-												mn.localVariables.size());
-										TryCatchBlockNode tryBlock = new TryCatchBlockNode(begin, end, handler,
-												"L" + StringUtils.randomAlphaNumericString(70) + ";");
-										mn.localVariables.add(ex);
-										mn.tryCatchBlocks.add(tryBlock);
-										mn.exceptions.add("java/lang/Throwable");
+											mn.instructions.add(new LdcInsnNode(getANSI()));
+
+											mn.instructions.add(new LdcInsnNode(
+													getANSI() + StringUtils.randomAlphaNumericString(16)));
+											mn.instructions.add(new InsnNode(Opcodes.ATHROW));
+
+											LocalVariableNode ex = new LocalVariableNode(
+													(Crasher ? classNode.name : FAKE_Handler), "L" + FAKE_Handler + ";",
+													null, begin, handler, mn.localVariables.size());
+											TryCatchBlockNode tryBlock = new TryCatchBlockNode(begin, end, handler,
+													"L" + (Crasher ? classNode.name : FAKE_Handler) + ";");
+											mn.localVariables.add(ex);
+											mn.tryCatchBlocks.add(tryBlock);
+											mn.exceptions.add(FAKE_Handler);
+										}
 									}
+
+									counter.incrementAndGet();
 								}
-								counter.incrementAndGet();
 							}
 						});
 		});
 
 		LoggerUtils.stdOut(String.format("%d FakeTryCatch Added.", counter.get()));
+		LoggerUtils.stdOut(String.format("%d Method Shiped.", counter_Shiped.get()));
 	}
 
 	@Override
@@ -71,5 +102,38 @@ public class FakeTryCatch extends Transformer {
 	@Override
 	public String getName() {
 		return "FakeTryCatch";
+	}
+
+	public ClassNode genHandler(String na) {
+		ClassNode classNode = createClass(na);
+		ClassWriter cw = new ClassWriter(0);
+		cw.newUTF8("Radon_Fake_TryCatch_Extend");
+		classNode.accept(cw);
+
+		this.getResources().put(classNode.name + ".class", cw.toByteArray());
+		return classNode;
+	}
+
+	private ClassNode createClass(String className) {
+		ClassNode classNode = new ClassNode();
+		classNode.visit(49, ACC_SUPER + ACC_PUBLIC, className, null, "java/lang/Throwable", null);
+
+		MethodVisitor mv = classNode.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+		mv.visitCode();
+		mv.visitVarInsn(ALOAD, 0);
+		mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Throwable", "<init>", "()V", false);
+		mv.visitInsn(RETURN);
+		mv.visitMaxs(1, 1);
+		mv.visitEnd();
+
+		classNode.visitEnd();
+
+		return classNode;
+	}
+
+	public static String getANSI() {
+		// Magic. this is no empty
+		// but some ANSI Control chars.
+		return "‫‮‮‌‬⁫";
 	}
 }
