8a3e4def98930ec58dd4c1a565728e8330258aa6
ItzSomebody <23221108+ItzSomebody@users.noreply.github.com>
2019-03-27 22:49:33 -0700

    Remove TODO in RandomUtils

--- radon-program/src/main/java/me/itzsomebody/radon/utils/RandomUtils.java
+++ radon-program/src/main/java/me/itzsomebody/radon/utils/RandomUtils.java
@@ -22,7 +22,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Used to generate various randoms.
- * TODO: rewrite this absolute garbage.
  *
  * @author ItzSomebody
  * @author freeasbird
